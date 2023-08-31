c032c7413a45489ad4e1404da011ff7b9cbc85dc
Mahammadmustafa A. Malikov <mahammadmustafaam@code.local>
2023-08-31 14:26:57 +0400

    second commit

---
+++ EternaApp/EternaApp/wwwroot/assets/js/custom.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    let path = window.location.pathname.split('/')[1];
+
+    console.log(path)
+    if (path == ""){
+        $('#Home').addClass('active')
+    }
+    else {
+        $('#' + path).addClass('active')
+    }
+})
